5cace286798fd1901a49ee3dcddd64f18bec6505
Alexey Ivanov <4985900@gmail.com>
2020-11-25 14:17:13 +0300

    fix: заэкранировал названия категорий

--- templates/main.php
+++ templates/main.php
@@ -10,8 +10,8 @@
         снаряжение.</p>
     <ul class="promo__list">
         <?php foreach ($categories as $category): ?>
-            <li class="promo__item promo__item promo__item--<?=$category['css_modifier']?>">
-                <a class="promo__link" href="./pages/all-lots.html"><?=$category['name'] ?></a>
+            <li class="promo__item promo__item promo__item--<?= $category['css_modifier'] ?>">
+                <a class="promo__link" href="./pages/all-lots.html"><?= htmlspecialchars($category['name']) ?></a>
             </li>
         <?php endforeach; ?>
     </ul>
