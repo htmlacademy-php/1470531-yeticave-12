3228d837c88a65ad0afb10265fdcbf0b8c298ca2
Alexey Ivanov <4985900@gmail.com>
2020-12-11 15:10:12 +0300

    feat: получение списков лотов и катеогорий для главной страницы из БД

--- index.php
+++ index.php
@@ -3,18 +3,47 @@
 include_once 'helpers.php';
 include_once 'config.php';
 
-$main_page_content = include_template('main.php', [
-    'categories' => $categories,
-    'offers' => $offers,
-]);
-$layout_content = include_template('layout.php', [
-    'title' => $page_titles['main'],
-    'is_auth' => $is_auth,
-    'user_name' => $user_name,
-    'content' => $main_page_content,
-    'categories' => $categories,
-]);
+if (!$mysql) {
+    $error = mysqli_connect_error();
+} else {
+    $categories_query = "SELECT `id`, `title`, `symbol_code` FROM yeticave.categories";
+    $offers_query .= "SELECT l.title                              title,
+                           l.starting_price                       starting_price,
+                           IFNULL(MAX(b.price), l.starting_price) current_price,
+                           l.image                                image,
+                           l.completion_date                      completion_date,
+                           c.title                                category
+                    FROM yeticave.lots l
+                             JOIN yeticave.categories c on c.id = l.category_id
+                             LEFT JOIN yeticave.bets b on l.id = b.lot_id
+                    WHERE l.completion_date > NOW()
+                    GROUP BY l.title, l.starting_price, l.image, l.created_on, c.title, l.completion_date
+                    ORDER BY l.created_on DESC";
+    $categories_res = mysqli_query($mysql, $categories_query);
+    $offers_res = mysqli_query($mysql, $offers_query);
 
-print($layout_content);
+
+    if ($categories_res) {
+        $categories = mysqli_fetch_all($categories_res, MYSQLI_ASSOC);
+    }
+
+    if ($offers_res) {
+        $offers = mysqli_fetch_all($offers_res, MYSQLI_ASSOC);
+    }
+
+    $main_page_content = include_template('main.php', [
+        'categories' => $categories,
+        'offers' => $offers,
+    ]);
+    $layout_content = include_template('layout.php', [
+        'title' => $page_titles['main'],
+        'is_auth' => $is_auth,
+        'user_name' => $user_name,
+        'content' => $main_page_content,
+        'categories' => $categories,
+    ]);
+
+    print($layout_content);
+}
 
 ?>
