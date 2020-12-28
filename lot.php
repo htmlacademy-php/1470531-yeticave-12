1b2fec0053a07e2a06e9bafd90de56d01b2600e8
Alexey Ivanov <4985900@gmail.com>
2020-12-28 16:02:25 +0300

    fix: поправил проверки

--- lot.php
+++ lot.php
@@ -6,25 +6,20 @@ include_once 'config.php';
 $categories = getCategories($mysql);
 $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
-if (strlen($id)) {
-    $offer = getOfferById($mysql, $id);
-}
-
-if ($offer['id']) {
-    $page_content = include_template('lot.php', [
-        'categories' => $categories,
-        'offer' => $offer,
-    ]);
-    $is_error = false;
-} else {
-    $page_content = '';
-    $is_error = true;
-}
-
-if ($is_error) {
+if (!$id || $id < 0) {
     redirect_to_404();
 }
 
+$offer = getOfferById($mysql, $id);
+
+if (!count($offer) || !$offer['id']) {
+    redirect_to_404();
+}
+
+$page_content = include_template('lot.php', [
+    'categories' => $categories,
+    'offer' => $offer,
+]);
 
 $layout_content = include_template('layout.php', [
     'title' => $offer['title'],
