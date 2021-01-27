0c6c73b9df557d6dcc95bb807076e4f216622945
Alexey Ivanov <4985900@gmail.com>
2021-01-27 09:42:54 +0300

    fix: смержил массивы в $page_content для компактности

--- login.php
+++ login.php
@@ -16,20 +16,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $errors = [];
     $errors['email'] = check_email($form['email']);
     $errors['password'] = check_text($form['password']);
+    $page_data = [
+        'categories' => $categories,
+        'form' => $form,
+        'errors' => $errors
+    ];
 
     if (count(array_filter($errors))) {
-        $page_content = include_template("login.php", [
-            'categories' => $categories,
-            'form' => $form,
-            'errors' => $errors
-        ]);
+        $page_content = include_template("login.php", $page_data);
     } else if (!is_user_exist($mysql, $form['email'])) {
         $errors['email'] = 'Пользователь с этим email не зарегистрирован';
-        $page_content = $page_content = include_template("login.php", [
-            'categories' => $categories,
-            'form' => $form,
-            'errors' => $errors
-        ]);;
+        $page_content = include_template("login.php", array_merge($page_data, ['errors' => $errors]));
     } else {
         $user = get_user($mysql, $form['email']);
 
@@ -40,11 +37,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
 
         $errors['password'] = 'Пароль неверный';
-        $page_content = $page_content = include_template("login.php", [
-            'categories' => $categories,
-            'form' => $form,
-            'errors' => $errors
-        ]);;
+        $page_content = include_template("login.php", array_merge($page_data, ['errors' => $errors]));
     }
 } else {
     if (isset($_SESSION['user'])) {
@@ -52,11 +45,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit();
     }
 
-    $page_content = $page_content = include_template("login.php", [
+    $page_content = include_template("login.php", [
         'categories' => $categories,
         'form' => [],
         'errors' => []
-    ]);;
+    ]);
 }
 
 $layout_content = include_template('layout.php', [
