090d483b34c0edba3e0aafbaabe5a915b091d836
Alexey Ivanov <4985900@gmail.com>
2021-02-05 14:43:48 +0300

    feat: добавил проверку, что id категории из запроса существует в массиве категорий

--- lots.php
+++ lots.php
@@ -10,6 +10,11 @@ if (!$category_id || intval($category_id) <= 0) {
 }
 
 $categories = getCategories($mysql);
+
+if (!array_key_exists(intval($category_id), $categories)) {
+    redirect_to_404();
+}
+
 $current_page = $_GET['page'] ?? 1;
 $page_items = 9;
 $items_count = count_offers_in_category($mysql, $category_id);
