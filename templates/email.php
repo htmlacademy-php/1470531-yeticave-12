fb36dbd0adaa72f06b185fbac346f540fd4b67b6
Alexey Ivanov <4985900@gmail.com>
2021-02-10 07:43:47 +0300

    ref: вынес адрес сайта для рассылки в константу

--- templates/email.php
+++ templates/email.php
@@ -7,10 +7,10 @@
 <h1>Поздравляем с победой</h1>
 <p>Здравствуйте, <?= $data['name'] ?></p>
 <p>Ваша ставка для лота
-    <a href="http://localhost:8090/lot.php?id=<?= $data['id'] ?>">
+    <a href="<?= SITE_PATH ?>/lot.php?id=<?= $data['id'] ?>">
         <?= $data['title'] ?>
     </a> победила.
 </p>
-<p>Перейдите по ссылке <a href="http://localhost:8090/my-bets.php">мои ставки</a>,
+<p>Перейдите по ссылке <a href="<?= SITE_PATH ?>/my-bets.php">мои ставки</a>,
     чтобы связаться с автором объявления</p>
 <small>Интернет Аукцион "YetiCave"</small>
